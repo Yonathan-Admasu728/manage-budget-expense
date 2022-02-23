710d729e16c8ce758c47ecf23e9b6a3eede9ad1c
Yonathan  Admasu <75287408+Yonathan-Admasu728@users.noreply.github.com>
2022-02-22 16:16:42 -0800

    implemented AddBudgetModal component and also connected it with the addBudget button on submit

--- budget-expense/src/App.js
+++ budget-expense/src/App.js
@@ -1,12 +1,17 @@
 import  Container  from "react-bootstrap/Container";
 import { Button, Stack } from "react-bootstrap";
 import BudgetCard from "./components/BudgetCard";
+import AddBudgetModal from "./components/AddBudgetModal";
+import { useState } from "react";
 
 function App() {
-  return <Container className="my-4">
+  const [showAddBudgetModal, setShowAddBudgetModal] = useState(false)
+  return (
+  <>
+  <Container className="my-4">
     <Stack direction="horizontal" gap="2" className="mb-4">
       <h1 className="me-auto">Budgets</h1>
-      <Button variant="primary">Add Budget</Button>
+      <Button variant="primary" onClick={() => setShowAddBudgetModal(true)}>Add Budget</Button>
       <Button variant="outline-primary">Add Expense</Button>
     </Stack>
     <div style={{display: "grid", girdTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))", gap: "1rem", alignItems: "flex-start" }}
@@ -14,6 +19,9 @@ function App() {
       <BudgetCard name="Entertainment" amount={200} max={1000} ></BudgetCard>
     </div>
   </Container>
+  <AddBudgetModal show={showAddBudgetModal} handleClose={() => setShowAddBudgetModal(false)} />
+  </>
+  )
     
 }
 
