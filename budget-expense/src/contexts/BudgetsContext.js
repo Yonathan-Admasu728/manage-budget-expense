d1a0a48de033f16e7fb4ab26624680d420a99834
Yonathan  Admasu <75287408+Yonathan-Admasu728@users.noreply.github.com>
2022-03-02 00:56:50 -0800

    implemented add budget and expense modal handeled the deleted expenses with uncategorized budget and also fixed the bug in the budgetCard

--- budget-expense/src/contexts/BudgetsContext.js
+++ budget-expense/src/contexts/BudgetsContext.js
@@ -1,71 +1,66 @@
-import React, { useContext } from "react";
-import { v4 as uuidV4 } from 'uuid';
-import useLocalStorage from "../hooks/useLocalStorage";
+import React, { useContext} from "react"
+import { v4 as uuidV4 } from "uuid"
+import useLocalStorage from "../hooks/useLocalStorage"
 
 const BudgetsContext = React.createContext()
 
+export const UNCATEGORIZED_BUDGET_ID = "Uncategorized"
+
 export function useBudgets() {
-   return useContext(BudgetsContext)
+  return useContext(BudgetsContext)
 }
 
-// {
-//     id:
-//     name:
-//     max:
-// }
-
-// {
-//     id: 
-//     budgetId: 
-//     amount: 
-//     description: 
-// }
-
 export const BudgetsProvider = ({ children }) => {
-    const [budgets, setBudgets] = useLocalStorage("budgets", [])
-    const [expenses, setExpenses]= useLocalStorage("expenses", [])
+  const [budgets, setBudgets] = useLocalStorage("budgets", [])
+  const [expenses, setExpenses] = useLocalStorage("expenses", [])
 
-    function getBudgetExpenses(budgetId) {
-        return expenses.filter(expense => expense.budgetId === budgetId)
-    }
-    function addExpenses({ description, amount, budgetId }) {
-        setExpenses(prevExpenses => {
-            return [...prevExpenses, { id: uuidV4(), description, amount, budgetId }]
-          })
+  function getBudgetExpenses(budgetId) {
+    return expenses.filter(expense => expense.budgetId === budgetId)
+  }
+  function addExpense({ description, amount, budgetId }) {
+    setExpenses(prevExpenses => {
+      return [...prevExpenses, { id: uuidV4(), description, amount, budgetId }]
+    })
+  }
+  function addBudget({ name, max }) {
+    setBudgets(prevBudgets => {
+      if (prevBudgets.find(budget => budget.name === name)) {
+        return prevBudgets
+      }
+      return [...prevBudgets, { id: uuidV4(), name, max }]
+    })
+  }
+  function deleteBudget({ id }) {
+    setExpenses(prevExpenses => {
+      return prevExpenses.map(expense => {
+        if (expense.budgetId !== id) return expense
+        return { ...expense, budgetId: UNCATEGORIZED_BUDGET_ID }
+      })
+    })
 
-    }
-    function addBudget({ name, max }) {
-        setBudgets(prevBudgets => {
-          if (prevBudgets.find(budget => budget.name === name)){
-              return prevBudgets
-          }
-          return [...prevBudgets, { id: uuidV4(), name, max }]
-        })
-    }
-    function deleteBudget({ id }) {
-        // TODO: deal with uncatagorized expenses when deleting budget
-        setBudgets(prevBudgets => {
-            return prevBudgets.filter(budget => budget.id !== id)
-        })
-    }
-    function deleteExpense({ id }) {
-        setExpenses(prevExpenses => {
-            return prevExpenses.filter(expense => expense.id !== id )
-        })
-    }
-
+    setBudgets(prevBudgets => {
+      return prevBudgets.filter(budget => budget.id !== id)
+    })
+  }
+  function deleteExpense({ id }) {
+    setExpenses(prevExpenses => {
+      return prevExpenses.filter(expense => expense.id !== id)
+    })
+  }
 
   return (
-      <BudgetsContext.Provider value={{
-          budgets,
-          expenses,
-          getBudgetExpenses,
-          addExpenses,
-          addBudget,
-          deleteBudget,
-          deleteExpense
-      }}>
-          {children}
-      </BudgetsContext.Provider>
+    <BudgetsContext.Provider
+      value={{
+        budgets,
+        expenses,
+        getBudgetExpenses,
+        addExpense,
+        addBudget,
+        deleteBudget,
+        deleteExpense,
+      }}
+    >
+      {children}
+    </BudgetsContext.Provider>
   )
 }
